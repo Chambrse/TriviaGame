d6c442fcbaef4f1c12a28c985579b2363eee994e
Chambrse <shaneechambry@gmail.com>
2018-05-31 11:07:48 -0700

    gamestuff styling

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,14 +1,12 @@
 $(document).ready(function () {
 
-    var questions = {
-        Q1: ["question #1", [option1,option2,option3], 0],
-        Q2: ["question #2", [option1,option2,option3], 1],
-    };
+/*     var questions = {
+        Q1: ["question #1", ["option1","option2","option3"], 0],
+        Q2: ["question #2", ["option1","option2","option3"], 1],
+    }; */
 
     $("#startButton").on("click", function() {
-        $("#gameArea").text("HelloWorld!")
+        $("#gameArea").html("test");
     });
-
-
 
 });
