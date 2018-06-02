1e9bc8b40ad7833d6e42c973b1c6f28068361716
Chambrse <shaneechambry@gmail.com>
2018-06-01 18:20:52 -0700

    added answerArea, functions ransomquestion, nextquestion, answerScreen

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,32 +1,89 @@
 $(document).ready(function () {
 
-/*     var questions = {
-        Q1: ["question #1", ["option1","option2","option3"], 0],
-        Q2: ["question #2", ["option1","option2","option3"], 1],
-    }; */
+    var questions = {
+        Q1: ["question #1", true],
+        Q2: ["question #2", false],
+    };
 
-    function timer () {
-        var currentTime = 30;
+    function timer() {
+
+        /* Set the timer at zero and print it in the appropriate div */
+        var currentTime = 10;
         $("#timer").text("Time remaining: " + currentTime);
 
-        setInterval(function() {
+        /* Every second, decrement the time remaining by one, print to div. */
+            timerInterval = setInterval(function () {
+
             currentTime--;
             $("#timer").text("Time remaining: " + currentTime);
+
+            /* Stop the timer at zero */
+            if (currentTime === 0) {
+                answerScreen();
+            }
+
         }, 1000);
+
+    };
+
+    function answerScreen(inputAnswer) {
+
+        /* Stop the timer */
+        clearInterval(timerInterval);
+
+        /* Show the answer area */
+        $("#gameArea").hide();
+        $("#answerArea").show();
+
+        if (questions[thisQuestion][1] === inputAnswer) {
+            $("#correctOrIncorrect").text("Nice! You got it right!");
+        } else {
+            $("#correctOrIncorrect").text("Wrong!");
+        }
+
+        $("#correctAnswer").text(questions[thisQuestion][1]);
+
+    };
+
+    function nextQuestion() {
+        $("#gameArea").show();
+        $("#answerArea").hide();
+        timer();
+        $("#question").text(questions[randomQuestion()][0]);
     }
 
-    $("#startButton").on("click", function() {
+
+    var questionsAsked = [0];
+    var qNumber = 0;
+    var thisQuestion;
+
+    function randomQuestion() {
+        while (questionsAsked.length < Object.keys(questions).length + 1 && questionsAsked.includes(qNumber)) {
+            console.log("test");
+            qNumber = Math.ceil(Math.random() * Object.keys(questions).length);
+        }
+        questionsAsked.push(qNumber);
+        thisQuestion = "Q" + qNumber;
+        console.log(qNumber);
+        return thisQuestion;
+    }
+
+
+
+    $("#startButton").on("click", function () {
 
         /* kill the header and show the game board. */
         $("#gameArea").show();
         $("#header").hide();
 
-        /* Start the timer */
+        /* Start the timer and add the first question */
         timer();
+        $("#question").text(questions[randomQuestion()][0]);
 
+    });
 
-
-        console.log("test");
+    $("#gameArea > .row > .col > #trueButton").on("click", function() {
+        answerScreen(true);
     });
 
 });
