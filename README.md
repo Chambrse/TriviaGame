4a29719ba3c37fdf9bacdd5a36efed243b09a606
Chambrse <shaneechambry@gmail.com>
2018-06-05 09:45:06 -0700

    added reset button, added some questions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TriviaGame
 A game of wills and wits.
+
+A basic trivia game to work out those  javascript, html, and CSS muscles with a focus on timers.
--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,29 +1,56 @@
 $(document).ready(function () {
 
+    /* Number of correct and incorrect answers */
+    var correctNumber = 0;
+    var incorrectNumber = 0;
+
+    /*  */
+    var questionsAsked = [0];
+    var qNumber = 0;
+    var thisQuestion;
+
+    function restart() {
+        correctNumber = 0;
+        incorrectNumber = 0;
+        questionsAsked = [0];
+        qNumber = 0;
+        thisQuestion = '';
+
+        $("#endArea").hide();
+        $("#gameArea").show();
+
+        timer();
+        $(".question").html("<h3>" + questions[randomQuestion()][0] + "</h3>");
+
+    };
+
     /* The questions */
     var questions = {
         Q1: ["'The Incredible Hulk' was the first official film in the Marvel Cinematic Universe.", true],
         Q2: ["Captain America's shield and Black Panther's suit are made of adamantium.", false],
         Q3: ["The power source of Iron Man's suit is called a tesseract.", false],
         Q4: ["The names of the infinity stones are as follows: Space Stone, Power Stone, Reality Stone, Time Stone, Mind Stone, Soul Stone.", true],
-        Q5: ["The technology that allows Ant-Man to change size is called the PIM particle.", true],
+        Q5: ["The technology that allows Ant-Man to change size is called the PYM particle.", true],
+        Q6: ["The Hulk gained his powers from a botched form of the super-soldier serum that was given to Captain America.", true],
+        Q7: ["Thor's hammer, Mjolnir, can only be wielded by himself.", false],
+        Q8: ["Star Lord is able to harness the power stone briefly without exploding because he is half celestial.", true],
+        Q9: ["In the guardians of the galaxy, Rocket uses a prosthetic leg to help the team escape from prison.", false],
+        Q10: ["At the end of The Avengers, the team gets some pizza together after the battle of New York.", false],
+        Q11: ["'Knowhere' is a city built inside the giant head of a dead celestial being.", true],
+        Q12: ["Nebula and Gamora have cybornetic enhancements provided to them by S.H.I.E.L.D. in order to save their lives.", false],
     };
-
-    /* Number of correct and incorrect answers */
-    var correctNumber = 0;
-    var incorrectNumber = 0;
 
     function timer() {
 
         /* Set the timer at zero and print it in the appropriate div */
         var currentTime = 10;
-        $("#timer").text("Time remaining: " + currentTime);
+        $(".timer").html("<h4>" + "Time remaining: " + currentTime + "<h4>");
 
         /* Every second, decrement the time remaining by one, print to div. */
         timerInterval = setInterval(function () {
 
             currentTime--;
-            $("#timer").text("Time remaining: " + currentTime);
+            $(".timer").html("<h4>" + "Time remaining: " + currentTime + "<h4>");
 
             /* Stop the timer at zero */
             if (currentTime === 0) {
@@ -33,11 +60,6 @@ $(document).ready(function () {
         }, 1000);
 
     };
-
-    /*  */
-    var questionsAsked = [0];
-    var qNumber = 0;
-    var thisQuestion;
 
     /* Pick a random question from the remaining questions */
     function randomQuestion() {
@@ -51,7 +73,7 @@ $(document).ready(function () {
         if (questionsAsked.length === Object.keys(questions).length + 1) {
             return -1;
 
-        /* mark the question as used, and return the key to the appropriate question. */
+            /* mark the question as used, and return the key to the appropriate question. */
         } else {
             questionsAsked.push(qNumber);
             thisQuestion = "Q" + qNumber;
@@ -68,7 +90,7 @@ $(document).ready(function () {
             $("#gameArea").show();
             $("#answerArea").hide();
             timer();
-            $("#question").text(questions[questionNumber][0]);
+            $(".question").html("<h3>" + questions[questionNumber][0] + "</h3>");
         };
     }
 
@@ -93,7 +115,7 @@ $(document).ready(function () {
             $("#correctOrIncorrect").html("<h3>" + "Nice! You got it right!" + "</h3>");
             correctNumber++;
 
-        /* incorrect if you're wrong */
+            /* incorrect if you're wrong */
         } else {
             $("#correctOrIncorrect").html("<h3>" + "Wrong!" + "</h3>");
             incorrectNumber++;
@@ -105,7 +127,7 @@ $(document).ready(function () {
         /* wait 3 seconds, then go to the next question */
         setTimeout(function () {
             nextQuestion();
-        }, 3000);
+        }, 10000);
 
     };
 
@@ -125,7 +147,7 @@ $(document).ready(function () {
 
         /* Start the timer and add the first question */
         timer();
-        $("#question").text(questions[randomQuestion()][0]);
+        $(".question").html("<h3>" + questions[randomQuestion()][0] + "</h3>");
 
     });
 
@@ -135,6 +157,10 @@ $(document).ready(function () {
 
     $("#gameArea > .row > .col > #falseButton").on("click", function () {
         answerScreen(false);
+    });
+
+    $("#restart").on("click",function() {
+        restart();
     });
 
 });
